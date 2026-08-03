a4cc003c4ae04dcc10e9b9ea9d261303589ab241
shane00413-wq <shane00413@gmail.com>
2026-08-03 11:31:42 +0800

    new-docs

--- sidebar.config.js
+++ sidebar.config.js
@@ -18,6 +18,40 @@ export default [
     ]
   },
   {
+    "label": "Linux Development",
+    "items": [
+      {
+        "label": "Fundamentals",
+        "items": [
+          {
+            "autogenerate": {
+              "directory": "Linux/Fundamentals"
+            }
+          }
+        ],
+        "translations": {
+          "zh-cn": "基础入门"
+        }
+      },
+      {
+        "label": "Dev Tools",
+        "items": [
+          {
+            "autogenerate": {
+              "directory": "Linux/DevTools"
+            }
+          }
+        ],
+        "translations": {
+          "zh-cn": "开发工具"
+        }
+      }
+    ],
+    "translations": {
+      "zh-cn": "Linux 开发"
+    }
+  },
+  {
     "label": "CLI Tools",
     "items": [
       {
